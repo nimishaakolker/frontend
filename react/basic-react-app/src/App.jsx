09a8e82f3fd5247aecddf100a88573e2235ae531
Nimisha Akolker <nimishaakolker@gmail.com>
2025-09-01 00:47:25 +0530

    Add Project

---
+++ react/basic-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import Title from "./Title.jsx";
+import ProductTab from "./ProductTab.jsx";
+function App() {
+  return (
+    <div>
+      <Title></Title>
+      <ProductTab />
+    </div>
+  )
+
+
+ }
+
+export default App;
---
+++ react/basic-react-app/src/Title.jsx
@@ -0,0 +1,6 @@
+function Title() {
+    return (
+        <h3>Hii! I am the title</h3>
+    )
+}
+export default Title;
